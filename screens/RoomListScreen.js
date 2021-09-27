6b467c998fbbf7b5361ece666e48a799843c103c
cms <80998461+kieuoanh231@users.noreply.github.com>
2021-09-27 19:22:15 +0700

    flatlist

--- screens/RoomListScreen.js
+++ screens/RoomListScreen.js
@@ -1,46 +1,39 @@
 import React from "react";
-import { View, StyleSheet, ScrollView, Text } from "react-native";
-import { BLUE1 } from "../src/values/color";
-import styled from 'styled-components';
+import { View, StyleSheet, ScrollView, Text, Image } from "react-native";
+import { BLUE1, LIGHT_GRAY } from "../src/values/color";
+import styled from "styled-components";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
+
+import RoomImage from "../src/values/1407953244000-177513283.jpg";
 
 const RoomListScreen = function () {
   return (
     <ScrollView>
-      <View style={roomStyles.header}>
-        {/* <View>
-          <Text style={roomStyles.headerTextName}>Hotel's Name</Text>
-          <Text style={roomStyles.headerTextAddress}>Hotel's Address</Text>
-        </View>
-
-        <Icon name="bookmark" size={20} /> */}
-
-        <ViewRow>
-          <Text style={roomStyles.headerTextName}>Hotel's Name</Text>
-          <Text style={roomStyles.headerTextName}>Hotel's Name</Text>
-          <Text style={roomStyles.headerTextName}>Hotel's Name</Text>
-          {/* <TouchableOpacity activeOpacity={0.5}>
-              <View style={{ flexDirection: "row", marginLeft: 12 }}>
-                <Icon
-                  name={"map-marked-alt"}
-                  size={SEARCH_ICON_SIZE}
-                  color={BLUE1}
-                />
-                <LabelSearch
-                  style={homeStyles.mapString}
-                  ellipsizeMode="tail"
-                  numberOfLines={1}
-                >
-                  {SEARCH_MAP_STRING}
-                </LabelSearch>
-              </View>
-            </TouchableOpacity> */}
-          {/* <Button
-              title={SEARCH_BTN_STRING}
-              buttonStyle={homeStyles.searchBtn}
-            /> */}
+      <View>
+        <ViewRow style={[roomStyles.header, roomStyles.horizontal]}>
+          <Icon
+            style={[roomStyles.icon, roomStyles.opacity]}
+            name="arrow-circle-left"
+            size={20}
+          />
+          <View style={{ paddingLeft: 15 }}>
+            <Text style={roomStyles.headerTextName}>Hotel's Name</Text>
+            <Text style={roomStyles.headerTextAddress}>Hotel's Address</Text>
+          </View>
+          <View style={roomStyles.horizontal}>
+            <Icon2 style={roomStyles.icon} name="bookmark-outline" size={20} />
+            <Icon2 style={roomStyles.icon} name="heart-outline" size={20} />
+            <Icon2 style={roomStyles.icon} name="dots-vertical" size={20} />
+          </View>
         </ViewRow>
+        <Image
+          style={roomStyles.image}
+          source={RoomImage}
+          resizeMode="stretch"
+        />
       </View>
+      
     </ScrollView>
   );
 };
@@ -54,9 +47,8 @@ const ViewRow = styled.View`
 
 const roomStyles = StyleSheet.create({
   header: {
-    height: 40,
+    height: 50,
     backgroundColor: BLUE1,
-    justifyContent: "center",
     alignItems: "center",
   },
   headerTextName: {
@@ -68,7 +60,20 @@ const roomStyles = StyleSheet.create({
     fontSize: 10,
   },
   icon: {
-    position: "absolute",
+    marginLeft: 10,
+    color: LIGHT_GRAY,
+  },
+  image: {
+    flex: 1,
+    width: "auto",
+    height: 200,
+  },
+  horizontal: {
+    flexDirection: "row",
+    paddingRight: 10,
+  },
+  opacity: {
+    opacity: 0.8,
   },
 });
 
---
+++ src/components/hotel/Room.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, View, Text, FlatList } from "react-native"
+import styled from "styled-components"
+
+const Room = function(){
+    return(
+        <View>
+            
+
+        </View>
+    )
+}
+
+export default Room;
